1a9b1db2338b1bb66b2d4189fec583089375b49d
Markopolo619 <markmadi123@protonmail.com>
2021-04-01 22:13:20 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
-import random
-choice = ["rock", "paper", "scissors"]
+fullname = input("Welcome, please enter your full name: ")
+email = input("And your email as well: ")
+user = open('details.txt', 'w')
+user.write("Full-names: " + fullname)
+user.write("\nemail: " + email)
+user.close()
 
-player = input("Welcome to Rock,Paper,Scissors. Type one:  ")
-
-computer = random.choice(choice)
-
-if player == "rock" and computer == "rock":
-    print("You drawn")
-elif player == "rock" and computer == "scissors":
-    print("You won")
-elif player == "rock" and computer == "paper":
-    print("You lost")
-
-
-if player == "paper" and computer == "rock":
-        print("You won")
-elif player == "paper" and computer == "scissors":
-        print("You lost")
-elif player == "paper" and computer == "paper":
-        print("You drawn")
-
-
-if player == "scissors" and computer == "rock":
-    print("You lost")
-elif player == "scissors" and computer == "scissors":
-    print("You drawn")
-elif player == "scissors" and computer == "paper":
-    print("You won")
-
-
-if player != choice[0]:
-    print("Please choose: Rock, Paper and Scissors")
-elif player != choice[1]:
-    print("Please choose: Rock, Paper and Scissors")
-elif player != choice[2]:
-    print("Please choose: Rock, Paper and Scissors")
-
+input = input("Do you want to see the contents: ")
+if input == "yes":
+    users = open('details.txt', 'r')
+    details = users.read()
+    print(details)
+    users.close()
+else:
+    print("Tough luck to you, chump")
